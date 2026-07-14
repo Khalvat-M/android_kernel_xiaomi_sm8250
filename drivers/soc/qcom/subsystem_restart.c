mrsrimar22/android_kernel_qcom_sm8250

qcom: ssr: Force RELATED restart_level for modem
modem runs intensively when device is online,
using SYSTEM restart_level on modem when device resume
forces device to do hot_reboot with kernel_panic

use RELATED restart_level, so that modem gets a chance
to restart itself without relying on SSR workaround condition

Change-Id: 9a4b59b7bae0d2f47ef66c3e64498ded7f46c90b
Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

drivers/soc/qcom/subsystem_restart.c
@@ -1236,8 +1236,14 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return -EBUSY;
 	}
 
-	pr_info("Restart sequence requested for %s, restart_level = %s.\n",
-		name, restart_levels[dev->restart_level]);
+	if (!strcmp(name, "modem")) {
+		dev->restart_level = RESET_SUBSYS_COUPLED;
+		pr_warn("Restart sequence requested for %s, force restart_level to = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	} else {
+		pr_info("Restart sequence requested for %s, restart_level = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	}
 
 	if (disable_restart_work == DISABLE_SSR) {
 		pr_warn("subsys-restart: Ignoring restart request for %s\n",

Khalvat-M/android_kernel_xiaomi_sm8250

Change-Id: I274d99577c1aba96acc7dd3d7fdf2f13174d1be2

drivers/soc/qcom/subsystem_restart.c
@@ -1236,8 +1236,14 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return -EBUSY;
 	}
 
-	pr_info("Restart sequence requested for %s, restart_level = %s.\n",
-		name, restart_levels[dev->restart_level]);
+	if (!strcmp(name, "esoc0")) {
+		dev->restart_level = RESET_SUBSYS_COUPLED;
+		pr_warn("Restart sequence requested for %s, force restart_level to = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	} else {
+		pr_info("Restart sequence requested for %s, restart_level = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	}
 
 	if (disable_restart_work == DISABLE_SSR) {
 		pr_warn("subsys-restart: Ignoring restart request for %s\n",